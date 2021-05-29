fc72a2d94a0f12d3baaccd257b7e05c9b84d17fb
Emily Corso <ekcorso@gmail.com>
2021-05-28 17:25:20 -0700

    Set up button and text box UI.

--- BarbellMath/WeightSelectionViewController.swift
+++ BarbellMath/WeightSelectionViewController.swift
@@ -13,13 +13,19 @@ class WeightSelectionViewController: UIViewController {
     let weightText = UITextField()
     let weightButton = UIButton()
     
+    override func loadView() {
+        super.loadView()
+        
+        self.navigationItem.title = "Are we gunna lift?"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationItem.title = "Are we gunna lift?"
-        view.backgroundColor = .systemGray
+        view.backgroundColor = .white
     
         establishSubviews()
+        setConstraints()
     }
     
     func didTapWeightButton() {
@@ -31,19 +37,31 @@ class WeightSelectionViewController: UIViewController {
 extension WeightSelectionViewController {
     
     func establishSubviews() {
+        view.addSubview(weightText)
         weightText.placeholder = "Weight goes here"
         weightText.borderStyle = .line
         weightText.textColor = .black
         weightText.backgroundColor = .white
         weightText.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(weightText)
         
+        view.addSubview(weightButton)
         weightButton.setTitle("Show me the numbers!", for: UIControl.State.normal)
+        weightButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        weightButton.backgroundColor = .systemTeal
+        weightButton.layer.cornerRadius = 4
+        //weightButton.layer.borderWidth = 1
+        //weightButton.layer.borderColor = UIColor.gray.cgColor
         weightButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(weightText)
+        
     }
     
     func setConstraints() {
-        
+        NSLayoutConstraint.activate([
+            weightText.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0),
+            weightText.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
+            
+            weightButton.topAnchor.constraint(equalTo: weightText.bottomAnchor, constant: 10),
+            weightButton.centerXAnchor.constraint(equalTo: weightText.centerXAnchor, constant: 0),
+        ])
     }
 }
