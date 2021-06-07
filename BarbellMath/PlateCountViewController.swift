0bc7f602d4e1371463557983b9a507af15437c7b
Emily Corso <ekcorso@gmail.com>
2021-06-07 13:55:34 -0700

    Remove refresh and weight selection buttons from PCVC.

--- BarbellMath/PlateCountViewController.swift
+++ BarbellMath/PlateCountViewController.swift
@@ -31,41 +31,14 @@ class PlateCountViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Plate Calculator"
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(
-            barButtonSystemItem: .add, target: self, action: #selector(promptForWeight))
-        
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(setStartingValues))
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain , target: nil, action: nil)
         
         var plateMathCalculator = PlateMathCalculator()
         let plateMathQuantitites = plateMathCalculator.doPlateMath(totalWeight: totalWeight)
         updateLabels(plateQuantities: plateMathQuantitites)
     }
-    
-    @objc func promptForWeight() {
-        let alert = UIAlertController(title: "Enter the amount of weight you're going to lift", message: "(Feeling pretty cocky right now huh?)", preferredStyle: .alert)
-        
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        
-        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned self, alert] action in
-            if let answerField = alert.textFields?[0],
-                let answer = answerField.text,
-                let doubleAnswer = Double(answer) {
-                    self.submit(doubleAnswer)
-            } else {
-                assertionFailure("Failed to unwrap answer.")
-            }
-        }
-        
-        alert.addAction(submitAction)
-        
-        alert.addTextField(configurationHandler: { textField in
-            textField.placeholder = "Your weight in pounds here"
-        })
-        
-        self.present(alert, animated: true)
-    }
+
     
     func updateLabels(plateQuantities: PlateQuantities) {
         text45LbPlates.text = "(\(Int(plateQuantities.quantity45LbPlates))) 45lb plates"
@@ -81,15 +54,6 @@ class PlateCountViewController: UIViewController {
         var plateMathCalculator = PlateMathCalculator()
         let plateQuantities = plateMathCalculator.doPlateMath(totalWeight: totalWeight)
         updateLabels(plateQuantities: plateQuantities)
-    }
-    
-    @objc func setStartingValues() {
-        //This will never be seen because the reset button isn't necessary now. The user will just navigate back to the initial view.
-        text2_5LbPlates.text = "(0) 2.5lb plates"
-        text5LbPlates.text = "(0) 5lb plates"
-        text10LbPlates.text = "(0) 10lb plates"
-        text25LbPlates.text = "(0) 25lb plates"
-        text45LbPlates.text = "(0) 45lb plates"
     }
 }
 
