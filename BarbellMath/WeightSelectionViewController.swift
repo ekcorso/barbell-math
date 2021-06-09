97033a1c161226189ec866a055126f6565a98697
Emily Corso <ekcorso@gmail.com>
2021-06-09 16:30:01 -0700

    Add and organize variables for implementing stack views for weight, bar, and unit selection.

--- BarbellMath/WeightSelectionViewController.swift
+++ BarbellMath/WeightSelectionViewController.swift
@@ -15,12 +15,20 @@ class WeightSelectionViewController: UIViewController {
     let unitSelectionStackView = UIStackView()
     let unitLabel = UILabel()
     let unitSelector = UISegmentedControl(items: ["lbs", "kgs"])
-    let spacerView1 = UIView()
-    let spacerView2 = UIView()
+    let unitSpacerView1 = UIView()
+    let unitSpacerView2 = UIView()
 
     let weightSelectionStackView = UIStackView()
     let weightLabel = UILabel()
     let weightTextField = UITextField()
+    let weightSpacerView1 = UIView()
+    let weightSpacerView2 = UIView()
+    
+    let barSelectionStackView = UIStackView()
+    let barLabel = UILabel()
+    let barSelector = UISegmentedControl(items: ["45lb", "35lb"])
+    let barSpacerView1 = UIView()
+    let barSpacerView2 = UIView()
     
     let submitButton = UIButton()
     
@@ -61,18 +69,7 @@ class WeightSelectionViewController: UIViewController {
 extension WeightSelectionViewController {
     
     func establishSubviews() {
-
-        spacerView1.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(spacerView1)
-
-        spacerView2.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(spacerView2)
-        
-        view.addSubview(unitSelector)
-        unitSelector.apportionsSegmentWidthsByContent = true
-        unitSelector.selectedSegmentIndex = 0
-        unitSelector.translatesAutoresizingMaskIntoConstraints = false
-        
+        //Set up directionsLabel
         view.addSubview(directionsLabel)
         directionsLabel.text = "What do you want to lift?"
         directionsLabel.textColor = .black
@@ -80,6 +77,7 @@ extension WeightSelectionViewController {
         directionsLabel.numberOfLines = 0
         directionsLabel.translatesAutoresizingMaskIntoConstraints = false
         
+        //Set up weightSelectionStackView and it's subviews
         view.addSubview(weightTextField)
         weightTextField.placeholder = "Weight"
         weightTextField.textAlignment = .center
@@ -89,6 +87,34 @@ extension WeightSelectionViewController {
         weightTextField.keyboardType = .numberPad
         weightTextField.translatesAutoresizingMaskIntoConstraints = false
         
+        //Set up unitSelectionStackView and it's subviews
+        unitSpacerView1.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(unitSpacerView1)
+
+        unitSpacerView2.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(unitSpacerView2)
+        
+        //unitSelector needs to have a func added to log the choice and/or pass it on to the next view
+        view.addSubview(unitSelector)
+        unitSelector.apportionsSegmentWidthsByContent = true
+        unitSelector.selectedSegmentIndex = 0
+        unitSelector.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(unitSelectionStackView)
+        unitSelectionStackView.translatesAutoresizingMaskIntoConstraints = false
+        unitSelectionStackView.alignment = .center
+        unitSelectionStackView.axis = .horizontal
+        unitSelectionStackView.distribution = .equalSpacing
+        unitSelectionStackView.backgroundColor = .white
+        unitSelectionStackView.addArrangedSubview(unitSpacerView1)
+        unitSelectionStackView.addArrangedSubview(weightTextField)
+        unitSelectionStackView.addArrangedSubview(unitSelector)
+        unitSelectionStackView.addArrangedSubview(unitSpacerView2)
+        
+        //Set up barSelectionStackView and it's subviews
+        
+        
+        //Set up button
         view.addSubview(submitButton)
         submitButton.setTitle("Show me how to load it", for: UIControl.State.normal)
         submitButton.titleLabel?.textColor = .black
@@ -98,16 +124,6 @@ extension WeightSelectionViewController {
         submitButton.translatesAutoresizingMaskIntoConstraints = false
         submitButton.addTarget(self, action: #selector(didTapWeightButton), for: .touchUpInside)
         
-        view.addSubview(unitSelectionStackView)
-        unitSelectionStackView.translatesAutoresizingMaskIntoConstraints = false
-        unitSelectionStackView.alignment = .center
-        unitSelectionStackView.axis = .horizontal
-        unitSelectionStackView.distribution = .equalSpacing
-        unitSelectionStackView.backgroundColor = .white
-        unitSelectionStackView.addArrangedSubview(spacerView1)
-        unitSelectionStackView.addArrangedSubview(weightTextField)
-        unitSelectionStackView.addArrangedSubview(unitSelector)
-        unitSelectionStackView.addArrangedSubview(spacerView2)
     }
     
     func setConstraints() {
@@ -126,8 +142,8 @@ extension WeightSelectionViewController {
             weightTextField.widthAnchor.constraint(equalToConstant: 130),
             weightTextField.heightAnchor.constraint(equalToConstant: 40),
                         
-            spacerView1.widthAnchor.constraint(equalToConstant: 10),
-            spacerView2.widthAnchor.constraint(equalToConstant: 10),
+            unitSpacerView1.widthAnchor.constraint(equalToConstant: 10),
+            unitSpacerView2.widthAnchor.constraint(equalToConstant: 10),
 
             submitButton.topAnchor.constraint(equalTo: unitSelectionStackView.bottomAnchor, constant: 12),
             submitButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
