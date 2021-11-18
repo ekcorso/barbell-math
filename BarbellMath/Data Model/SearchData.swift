71e07b5226818c643a5b228da429794ff61f2bd4
Emily Corso <ekcorso@gmail.com>
2021-11-18 11:53:49 -0800

    Add SearchData and SearchResult structs

---
+++ BarbellMath/Data Model/SearchData.swift
@@ -0,0 +1,16 @@
+//
+//  SearchData.swift
+//  BarbellMath
+//
+//  Created by Emily Corso on 11/18/21.
+//  Copyright © 2021 Emily Corso. All rights reserved.
+//
+
+import Foundation
+
+struct SearchData {
+    var weight: Double!
+    var units: String!
+    var barWeight: Int!
+    var result: SearchResult?
+}
---
+++ BarbellMath/Data Model/SearchResult.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResult.swift
+//  BarbellMath
+//
+//  Created by Emily Corso on 11/18/21.
+//  Copyright © 2021 Emily Corso. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult {
+    var barLoadInLbs: BarLoadInLbs?
+    var barLoadInKgs: BarLoadInKgs?
+    var quantityOfCats: Double
+}
