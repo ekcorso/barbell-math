783280a3b1f9a0455a958cd34edf37d955d85c36
Emily Corso <ekcorso@gmail.com>
2021-11-21 20:06:40 -0800

    Move allSearches initialization to ViewWillAppear

--- BarbellMath/Views/WeightSelectionViewController.swift
+++ BarbellMath/Views/WeightSelectionViewController.swift
@@ -233,12 +233,13 @@ class WeightSelectionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .systemBackground
         
-        self.allSearches = DataStorage().retrieve()
-        
         setConstraints()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.allSearches = DataStorage().retrieve()
     }
     
     // MARK: - Actions
