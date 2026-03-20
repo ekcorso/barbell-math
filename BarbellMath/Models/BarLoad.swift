95733088833717cbfe6137932a588528abea273c
Emily Corso <ekcorso@gmail.com>
2026-03-20 11:47:00 -0700

    Consolidate bridging BarLoad and PlateLoad types to the BarLoad file

--- BarbellMath/Models/BarLoad.swift
+++ BarbellMath/Models/BarLoad.swift
@@ -1,5 +1,5 @@
 //
-//  LegacyBarLoad.swift
+//  BarLoad.swift
 //  BarbellMath
 //
 //  Created by Emily Corso on 1/10/21.
@@ -24,4 +24,21 @@ struct BarLoadInKgs: Codable {
     var quantity5KgPlates: Double = 0
     var quantity2_5KgPlates: Double = 0
     var quantity1_25KgPlates: Double = 0
+}
+
+// Replaces the legacy BarLoadInKgs and BarLoadInLbs types
+struct BarLoad: Codable {
+    let plateLoadPerSize: [PlateLoad]
+    let quantityInCats: Int
+}
+
+struct PlateLoad: Codable {
+    let plateSize: Double
+    let quantity: Int // must be an even number
+    let units: Units
+    
+    // Will need to move this to the PlateCountViewModel if there ends up being more display logic
+    var formattedLoad: String {
+        "(\(quantity)) \(plateSize.formatted())\(units) plates"
+    }
 }
