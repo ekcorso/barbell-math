999be72cc6112ffa640acc2986758348265652c3
Emily Corso <ekcorso@gmail.com>
2026-03-20 14:00:33 -0700

    Refator Validator class

--- BarbellMath/Helpers/Validator.swift
+++ BarbellMath/Helpers/Validator.swift
@@ -6,8 +6,9 @@
 //  Copyright © 2021 Emily Corso. All rights reserved.
 //
 
-import UIKit
+import UIKit // TODO: (Remove this too, Validator should not know about UI code)
 
+// TODO: Remove this and promote the extension to main declaration when the migration is complete
 class Validator {
     func isWholeNumber(userEntry: String) -> Bool {
         if let _ = Int(userEntry) {
@@ -40,4 +41,24 @@ class Validator {
         return alert
     }
     
+}
+
+// New -- should only need this part after the migration
+enum TotalValidationError: Error, Identifiable {
+    var id: Self { self }
+    
+    case notInteger
+    case notDivisibleByFive
+    case belowMinimumWeight
+}
+
+extension Validator {
+    // TODO: Update this logic for the following:
+    // kg users can enter multiples of .5, not just Ints that are multiples of 5
+    // min weight should depend on bar and units
+    func isValid(total weight: Double) throws(TotalValidationError) {
+        guard weight == weight.rounded() else { throw .notInteger }
+        guard Int(weight) % 5 == 0     else { throw .notDivisibleByFive }
+        guard weight >= 50             else { throw .belowMinimumWeight }
+    }
 }
