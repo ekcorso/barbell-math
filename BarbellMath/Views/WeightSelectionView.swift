2c5a95cdf06729c37e46831751ebea1eeddec8f3
Emily Corso <ekcorso@gmail.com>
2026-03-31 17:29:36 -0700

    Extract hiz padding to a variable

--- BarbellMath/Views/WeightSelectionView.swift
+++ BarbellMath/Views/WeightSelectionView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct WeightSelectionView: View {
     @StateObject private var viewModel = WeightSelectionViewModel()
-
+    let horizontalPadding = 20.0
     
     var body: some View {
         NavigationStack {
